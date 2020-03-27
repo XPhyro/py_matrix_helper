ce1316aa1ce19ea3ac803f3a1a4fce1997557b8d
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-03-27 05:32:48 +0300

    Add unit tests to flatten_matrix

--- __init__.py
+++ __init__.py
@@ -3,4 +3,14 @@
 
 
 def flatten_mat(mat):
+    if mat is not list:
+        raise TypeError("mat must be a list.")
+    for i in mat:
+        if i is not list:
+            raise TypeError("Each element of mat must be a list.")
+    inner_len = len(mat[0])
+    for i in mat:
+        if len(i) != inner_len:
+            raise ValueError("Each element of mat must be of same size, i.e. mat must be rectangular.")
+
     return [j for i in mat for j in i]
