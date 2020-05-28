39619bbdcae79191a7116639483dfed04bfc5d15
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-05-28 03:43:04 +0300

    Add init_mat(...)

--- __init__.py
+++ __init__.py
@@ -3,10 +3,10 @@
 
 
 def flatten_mat(mat):
-    if mat is not list:
+    if not isinstance(mat, list):
         raise TypeError("mat must be a list.")
     for i in mat:
-        if i is not list:
+        if not isinstance(i, list):
             raise TypeError("Each element of mat must be a list.")
     inner_len = len(mat[0])
     for i in mat:
@@ -16,3 +16,25 @@ def flatten_mat(mat):
             )
 
     return [j for i in mat for j in i]
+
+def init_mat(h, w, v):
+    if not isinstance(h, int):
+        raise TypeError("h must be an int.")
+    if not isinstance(w, int):
+        raise TypeError("w must be an int.")
+    if not isinstance(v, float):
+        raise TypeError("v must be a float.")
+
+    if h <= 0:
+        raise ValueError("h must be positive.")
+    if w <= 0:
+        raise ValueError("w must be positive.")
+
+    m = []
+    for i in range(h):
+        l = []
+        for i in range(w):
+            l.append(v)
+        m.append(l)
+
+    return m
